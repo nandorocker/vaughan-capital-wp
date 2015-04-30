aebbe1e5130a2857337fe9282cf350878528c05d
Nando Rossi <talk@nan.do>
2015-04-30 22:31:26 +0200

    Adds basic header image

--- wp-content/themes/vaughan-capital/content-page.php
+++ wp-content/themes/vaughan-capital/content-page.php
@@ -6,6 +6,19 @@
  */
 ?>
 
+<?php
+
+// Header Image
+if ( has_post_thumbnail() ) {
+	$post_thumbnail_id = get_post_thumbnail_id($post->ID);
+	$post_thumbnail_url = wp_get_attachment_url( $post_thumbnail_id );
+
+	echo "<figure class='header-image' style='background-image: url(" . $post_thumbnail_url . ");'>";
+		//the_post_thumbnail();
+	echo "</figure>";
+}
+
+?>
 <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
 	<header class="entry-header">
 		<?php the_title( '<h1 class="entry-title">', '</h1>' ); ?>
