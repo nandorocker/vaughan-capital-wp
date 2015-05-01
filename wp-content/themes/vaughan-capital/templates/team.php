ac53abb9f283ee2304ae0aedf39d1a352bb01107
Nando Rossi <talk@nan.do>
2015-05-01 02:53:09 +0200

    Initial dynamic team page

---
+++ wp-content/themes/vaughan-capital/templates/team.php
@@ -0,0 +1,70 @@
+<?php
+/**
+ * Template Name: Team page
+ *
+ * Displays content for transactions
+ *
+ * @package _mbbasetheme
+ */
+
+get_header(); ?>
+
+	<div id="primary" class="content-area">
+		<main id="main" class="site-main" role="main">
+
+			<?php 
+while ( have_posts() ) : the_post(); // begin current page loop  
+?>
+			<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+				<header class="entry-header">
+					<?php the_title( '<h1 class="entry-title">', '</h1>' ); ?>
+				</header><!-- .entry-header -->
+
+				<div class="entry-content">
+					<?php the_content(); // Display intro if found ?>
+
+					<?php
+	/*
+	** Team section
+	 */
+
+	// Create a custom wordpress query
+	$args = array(
+
+		'cat' => 9,
+		'posts_per_page' => -1,
+		'order' => 'DESC',
+		'orderby' => 'date',
+		'exclude' => 3
+	);
+
+	query_posts( $args );
+
+	// Start wordpress loop!
+	if (have_posts()) : while ( have_posts() ) : the_post(); 
+?>
+
+					<article class="team-mate">
+						<h2><?php the_title(); ?></h2>
+						<h3><?php the_field("role"); ?> - <?php the_field("homebase"); ?></h3>
+						<?php the_content(); ?>
+					</article>	
+
+					<?php
+		endwhile;
+	endif;
+	wp_reset_query(); // End category loop query
+?>
+
+				</div><!-- .entry-content -->
+			</article><!-- #post-## -->	
+
+			<?php 
+
+endwhile; // end of the main loop. 
+?>
+
+		</main><!-- #main -->
+	</div><!-- #primary -->
+
+<?php get_footer(); ?>
