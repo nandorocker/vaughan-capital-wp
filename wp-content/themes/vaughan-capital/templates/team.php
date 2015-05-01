c5e18826985f1deff2384d60eff672b5f42b2f39
Nando Rossi <talk@nan.do>
2015-05-01 03:42:51 +0200

    Adds underline to page titles

--- wp-content/themes/vaughan-capital/templates/team.php
+++ wp-content/themes/vaughan-capital/templates/team.php
@@ -17,7 +17,7 @@ while ( have_posts() ) : the_post(); // begin current page loop
 ?>
 			<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
 				<header class="entry-header">
-					<?php the_title( '<h1 class="entry-title">', '</h1>' ); ?>
+					<?php the_title( '<h2 class="entry-title sidebar-line">', '</h2>' ); ?>
 				</header><!-- .entry-header -->
 
 				<div class="entry-content">
