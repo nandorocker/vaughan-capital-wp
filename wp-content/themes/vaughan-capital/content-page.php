65542d1b4426828f563956e72af063ae1f3b3322
Nando Rossi <talk@nan.do>
2015-05-01 02:47:24 +0200

    Moves thumbnail to its own page (like an include)

--- wp-content/themes/vaughan-capital/content-page.php
+++ wp-content/themes/vaughan-capital/content-page.php
@@ -6,19 +6,8 @@
  */
 ?>
 
-<?php
+<?php get_template_part( 'content', 'thumbnail' ); ?>
 
-// Header Image
-if ( has_post_thumbnail() ) {
-	$post_thumbnail_id = get_post_thumbnail_id($post->ID);
-	$post_thumbnail_url = wp_get_attachment_url( $post_thumbnail_id );
-
-	echo "<figure class='header-image' style='background-image: url(" . $post_thumbnail_url . ");'>";
-		//the_post_thumbnail();
-	echo "</figure>";
-}
-
-?>
 <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
 	<header class="entry-header">
 		<?php the_title( '<h1 class="entry-title">', '</h1>' ); ?>
---
+++ wp-content/themes/vaughan-capital/content-thumbnail.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * The template part for displaying the page's banner.
+ *
+ * @package _mbbasetheme
+ */
+?>
+<?php
+	// Header Image
+	if ( has_post_thumbnail() ) {
+		$post_thumbnail_id = get_post_thumbnail_id($post->ID);
+		$post_thumbnail_url = wp_get_attachment_url( $post_thumbnail_id );
+
+		echo "<figure class='header-image' style='background-image: url(" . $post_thumbnail_url . ");'></figure>";
+	}
+?>
