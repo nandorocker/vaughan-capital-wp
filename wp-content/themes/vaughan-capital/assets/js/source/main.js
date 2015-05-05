e2ec88ae21814a5f2a4ee852e890e941e7f7bb65
nandorocker <talk@nan.do>
2015-05-05 16:07:36 -0300

    Limits backstretch to home

--- wp-content/themes/vaughan-capital/assets/js/source/main.js
+++ wp-content/themes/vaughan-capital/assets/js/source/main.js
@@ -1,6 +1,16 @@
 (function($) {
-	var t_dir = SiteParameters.theme_directory;
-	// alert(SiteParameters);
+	if (typeof SiteParameters != 'undefined') {
+		var t_dir = SiteParameters.theme_directory;
+
+		/*
+		 * Backstretch slideshow
+		 */
+		$.backstretch([
+			t_dir + "/assets/images/home-bg-la.jpg",
+			t_dir + "/assets/images/home-bg-sf.jpg",
+			t_dir + "/assets/images/home-bg-ny.jpg"
+	 	], {duration: 2500, fade: 750});
+	}
 
 	/*
 	 * Backstretch slideshow
